817aaf2a37135d9ad5898cab1df475d501abad51
Martin Knopp <MartinKnopp@gmx.de>
2021-06-23 01:03:04 +0200

    Get ORM relationships right

--- mcr_analyser/database/database.py
+++ mcr_analyser/database/database.py
@@ -9,25 +9,33 @@
 
 """ Database routines for setup and usage """
 
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# ORM interface
 Base = declarative_base()
-engine = create_engine("sqlite:///database.sqlite")
 
 
-def init_db():
-    from mcr_analyser.database.models import (
-        Chip,
-        Device,
-        Measurement,
-        Reagent,
-        Result,
-        Sample,
-        SampleType,
-        User,
-    )
+class Database:
+    def __init__(self, engine: str):
+        self.base = Base
+        self.engine = create_engine(engine)
+        self.Session = sessionmaker(bind=self.engine)
 
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
+    def empty_and_init_db(self):
+        import mcr_analyser.database.models  # noqa: F401
+
+        self.base.metadata.drop_all(bind=self.engine)
+        self.base.metadata.create_all(bind=self.engine)
+
+    def get_or_create(self, session, model, **kwargs):
+        instance = session.query(model).filter_by(**kwargs).one_or_none()
+        if instance:
+            return instance
+        else:
+            instance = model(**kwargs)
+            session.add(instance)
+            session.commit()
+            return instance
--- mcr_analyser/database/models.py
+++ mcr_analyser/database/models.py
@@ -34,7 +34,6 @@ class Chip(Base):
     __tablename__ = "chip"
     id: int = Column(Integer, primary_key=True)
     """Internal ID, used for cross-references."""
-    relationship("Measurement", back_populates="chip")
     name: str = Column(String)
     """Chip ID assigned by user during measurement."""
     rowCount: int = Column(Integer, nullable=False)
@@ -45,6 +44,11 @@ class Chip(Base):
     spotSize: int = Column(Integer, nullable=False)
     """Size (in pixels) of a single spot. Side length of the square used for
     processing."""
+    measurements: relationship = relationship(
+        "Measurement", back_populates="chip", order_by="Measurement.timestamp"
+    )
+    """Many-to-One relationship referencing all measurements the chip was
+    used for."""
 
 
 class Device(Base):
@@ -53,56 +57,73 @@ class Device(Base):
     __tablename__ = "device"
     id: int = Column(Integer, primary_key=True)
     """Internal ID, used for cross-references."""
-    relationship("Measurement", back_populates="device")
     serial: str = Column(String(255), nullable=False)
     """Serial number of the device."""
+    measurements: relationship = relationship(
+        "Measurement", back_populates="device", order_by="Measurement.timestamp"
+    )
+    """Many-to-One relationship referencing all measurements done with this
+    device."""
 
 
 class Measurement(Base):
     """A single measurement. This is the central table everything is about."""
 
     __tablename__ = "measurement"
-    id: str = Column(BINARY(32), primary_key=True)
+    id: bytes = Column(BINARY(32), primary_key=True)
     """SHA256 hash of the raw 16-bit image data. Used for duplicate detection
     and potential future database merges."""
     chipID: int = Column(Integer, ForeignKey("chip.id"))
     """Refers to the used :class:`Chip`."""
-    relationship("Chip", back_populates="measurement")
+    chip: relationship = relationship("Chip", back_populates="measurements")
+    """One-to-Many relationship referencing the used chip."""
     deviceID: int = Column(Integer, ForeignKey("device.id"))
     """Refers to the used :class:`Device`."""
-    relationship("Device", back_populates="measurement")
+    device: relationship = relationship("Device", back_populates="measurements")
+    """One-to-Many relationship referencing the used device."""
     sampleID: int = Column(Integer, ForeignKey("sample.id"))
     """Refers to the measured :class:`Sample`."""
-    relationship("Sample", back_populates="measurement")
+    sample: relationship = relationship("Sample", back_populates="measurements")
+    """One-to-Many relationship referencing the analysed sample."""
     image: bytes = Column(LargeBinary, nullable=False)
     """Raw 16-bit image data, big endian. (Numpy's ``>u2`` datatype, for
     compatibility with `netpbm <http://netpbm.sourceforge.net/doc/pgm.html>`_).
     """
-    timestamp: datetime.datetime = Column(DateTime)
+    timestamp: datetime.datetime = Column(DateTime, index=True)
     """Date and time of the measurement."""
     userID: int = Column(Integer, ForeignKey("user.id"))
     """Refers to the :class:`User` who did the measurement."""
-    relationship("User", back_populates="measurement")
+    user: relationship = relationship("User", back_populates="measurements")
+    """One-to-Many releationship referencing the user who did the
+    measurement."""
     chipFailure: bool = Column(Boolean, nullable=False, default=False)
     """Was there a failure during measurement (leaky chip). Defaults to
     `False`."""
     notes: str = Column(Text)
     """Additional notes."""
+    results: relationship = relationship(
+        "Result", back_populates="measurement", order_by="Result.id"
+    )
+    """Many-to-One relationship referncing all results of this measurement."""
 
 
 class Reagent(Base):
     """Substance used on a chip.
 
-    Depending on the use case this could be a
-    reagent which should be detected or a reagent put on the chip.
+    This class handles column specific information (for end-users), so depending
+    on what information is more useful, this is about the reagent which should
+    be detected or the reagent which is initially put on the chip.
     """
 
     __tablename__ = "reagent"
     id: int = Column(Integer, primary_key=True)
     """Internal ID, used for cross-references."""
-    relationship("Result", back_populates="reagent")
     name: str = Column(String)
-    """ Name of the substance."""
+    """Name of the substance."""
+    results: relationship = relationship(
+        "Result", back_populates="reagent", order_by="Result.id"
+    )
+    """Many-to-One relationship referencing the Spots of this substance."""
 
 
 class Result(Base):
@@ -113,16 +134,19 @@ class Result(Base):
     """Internal ID, used for cross-references."""
     measurementID: int = Column(Integer, ForeignKey("measurement.id"))
     """Reference to the :class:`Measurement` to which the result belongs."""
-    relationship("Measurement", back_populates="result")
+    measurement: relationship = relationship("Measurement", back_populates="results")
+    """One-to-Many relationship referencing the measurement which yielded this
+    result."""
     row: int = Column(Integer)
     """Row index, counted from 0."""
     column: int = Column(Integer)
     """Column index, counted from 0."""
     value: float = Column(Float)
     """Calculated brightness of the spot."""
-    reagent: int = Column(Integer, ForeignKey("reagent.id"))
+    reagentID: int = Column(Integer, ForeignKey("reagent.id"))
     """Reference to :class:`Reagent`."""
-    relationship("Reagent", back_populates="result")
+    reagent: relationship = relationship("Reagent", back_populates="results")
+    """One-to-Many relationship referencing the substance of this spot."""
     concentration: float = Column(Float)
     """Additional concentration information to specify the :attr:`reagent` more
     precisely."""
@@ -140,14 +164,21 @@ class Sample(Base):
     knownPositive: bool = Column(Boolean)
     """Is this a know positive sample? Makes use of the tri-state SQL bool
     `None`, `True`, or `False`."""
-    type: int = Column(Integer, ForeignKey("sampleType.id"))
+    typeID: int = Column(Integer, ForeignKey("sampleType.id"))
     """Refers to :class:`SampleType`."""
-    relationship("SampleType", back_populates="sample")
-    takenBy: int = Column(Integer, ForeignKey("user.id"))
+    type: relationship = relationship("SampleType", back_populates="samples")
+    """One-to-Many relationship referencing the type of this sample."""
+    takenByID: int = Column(Integer, ForeignKey("user.id"))
     """Refers to the :class:`User` who took the sample."""
-    relationship("User", back_populates="user")
+    takenBy: relationship = relationship("User", back_populates="samples")
+    """One-to-Many relationship referencing the user who took this sample."""
     timestamp: datetime.datetime = Column(DateTime)
     """Date and time of the sample taking."""
+    measurements: relationship = relationship(
+        "Measurement", back_populates="sample", order_by="Measurement.timestamp"
+    )
+    """Many-to-One relationship referencing the measurements done with this
+    sample."""
 
 
 class SampleType(Base):
@@ -156,9 +187,12 @@ class SampleType(Base):
     __tablename__ = "sampleType"
     id: int = Column(Integer, primary_key=True)
     """Internal ID, used for cross-references."""
-    relationship("Sample", back_populates="sampleType")
     name: str = Column(String, nullable=False)
     """Name of the kind. For example full blood, serum, water, etc."""
+    samples: relationship = relationship(
+        "Sample", back_populates="type", order_by="Sample.id"
+    )
+    """Many-to-One relationship referencing all samples of this type."""
 
 
 class User(Base):
@@ -167,8 +201,15 @@ class User(Base):
     __tablename__ = "user"
     id: str = Column(Integer, primary_key=True)
     """Internal ID, used for cross-references."""
-    relationship("Sample", back_populates="user")
     name: str = Column(String, nullable=False)
     """Name of the researcher."""
     loginID: str = Column(String)
     """User ID of the researcher, to be used for automatic association."""
+    samples: relationship = relationship(
+        "Sample", back_populates="takenBy", order_by="Sample.id"
+    )
+    """Many-to-One relationship referencing all samples taken by a user."""
+    measurements: relationship = relationship(
+        "Measurement", back_populates="user", order_by="Measurement.timestamp"
+    )
+    """Many-to-One relationship referencing all measurements done by a user."""
