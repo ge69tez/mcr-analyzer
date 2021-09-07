9b7585f5ac1926c4b85290c7814b95a93a8bfa52
Martin Knopp <MartinKnopp@gmx.de>
2021-09-07 18:55:38 +0200

    Importer: calculate SHA sums in separate thread

--- mcr_analyser/ui/importer.py
+++ mcr_analyser/ui/importer.py
@@ -11,13 +11,12 @@ import hashlib
 import numpy as np
 import sqlalchemy
 
-from qtpy import QtGui, QtWidgets
-from pathlib import Path
+from qtpy import QtCore, QtGui, QtWidgets
 
 from mcr_analyser.database.database import Database
 from mcr_analyser.database.models import Measurement
 from mcr_analyser.io.image import Image
-from mcr_analyser.io.importer import FileImporter, RsltParser
+from mcr_analyser.io.importer import FileImporter
 
 
 class ImportDialog(QtWidgets.QDialog):
@@ -25,6 +24,8 @@ class ImportDialog(QtWidgets.QDialog):
         super().__init__(parent)
         self.dirs = []
         self.file_model = None
+        self.thread = None
+        self.result_worker = None
         self.setWindowTitle(_("Import measurements"))
 
         layout = QtWidgets.QVBoxLayout()
@@ -42,6 +43,9 @@ class ImportDialog(QtWidgets.QDialog):
         self.measurements_table = QtWidgets.QTableView()
         layout.addWidget(self.measurements_table)
 
+        self.progress_bar = QtWidgets.QProgressBar()
+        layout.addWidget(self.progress_bar)
+
     def path_dialog(self):
         dialog = QtWidgets.QFileDialog(self)
         dialog.setFileMode(QtWidgets.QFileDialog.Directory)
@@ -52,24 +56,57 @@ class ImportDialog(QtWidgets.QDialog):
 
     def update_filelist(self):
         imp = FileImporter()
-        db = Database()
-        session = db.Session()
+
         self.file_model = QtGui.QStandardItemModel(self)
+        self.file_model.setHorizontalHeaderLabels(["Old/New", "Path"])
 
         for path in self.dirs:
             results = imp.gather_measurements(path)
 
+            self.progress_bar.setMaximum(len(results))
+
+            # Calculate image checksums in separate thread
+            self.thread = QtCore.QThread()
+            self.result_worker = ResultWorker(results)
+            self.result_worker.moveToThread(self.thread)
+            self.thread.started.connect(self.result_worker.run)
+            self.result_worker.progress.connect(self.updateStatus)
+            self.result_worker.finished.connect(self.thread.quit)
+            self.result_worker.finished.connect(self.result_worker.deleteLater)
+            self.result_worker.finished.connect(self.thread.deleteLater)
+            self.thread.start()
+
             for res in results:
-                img = Image(res.dir.joinpath(res.meta["Result image PGM"]))
-
-                sha = hashlib.sha256(np.ascontiguousarray(img.data, ">u2"))
-
-                try:
-                    session.query(Measurement).filter_by(id=sha.digest()).one()
-                    measurement = QtGui.QStandardItem(f"OLD: {res.path}")
-                except sqlalchemy.orm.exc.NoResultFound:
-                    measurement = QtGui.QStandardItem(f"NEW: {res.path}")
-
+                measurement = [
+                    QtGui.QStandardItem(""),
+                    QtGui.QStandardItem(f"{res.path}"),
+                ]
                 self.file_model.appendRow(measurement)
 
         self.measurements_table.setModel(self.file_model)
+
+    def updateStatus(self, step, hash):
+        db = Database()
+        session = db.Session()
+        self.progress_bar.setValue(step + 1)
+        try:
+            session.query(Measurement).filter_by(id=hash).one()
+            self.file_model.item(step, 0).setText("OLD")
+        except sqlalchemy.orm.exc.NoResultFound:
+            self.file_model.item(step, 0).setText("NEW")
+
+
+class ResultWorker(QtCore.QObject):
+    def __init__(self, results=list, parent=None):
+        super().__init__(parent)
+        self.results = results
+
+    finished = QtCore.Signal()
+    progress = QtCore.Signal(int, bytes)
+
+    def run(self):
+        for i, res in enumerate(self.results):
+            img = Image(res.dir.joinpath(res.meta["Result image PGM"]))
+            sha = hashlib.sha256(np.ascontiguousarray(img.data, ">u2"))
+            self.progress.emit(i, sha.digest())
+        self.finished.emit()
