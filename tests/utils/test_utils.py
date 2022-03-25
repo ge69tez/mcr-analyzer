5f98e8fb46e387f71e051388238b72645e323247
Martin Knopp <MartinKnopp@gmx.de>
2022-03-25 19:30:35 +0100

    Fix utils.simplify_list, add unittest

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -14,12 +14,12 @@ import mcr_analyser.utils as utils
 
 class TestEnsureList:
     def test_list(self):
-        input = [0, 1, 2]
-        assert isinstance(utils.ensure_list(input), list)
+        inp = [0, 1, 2]
+        assert isinstance(utils.ensure_list(inp), list)
 
     def test_tuple(self):
-        input = (0, 1, 2)
-        assert isinstance(utils.ensure_list(input), tuple)
+        inp = (0, 1, 2)
+        assert isinstance(utils.ensure_list(inp), tuple)
 
     def test_none(self):
         assert utils.ensure_list(None) == []
@@ -32,8 +32,8 @@ class TestEnsureList:
 
 class TestRemoveDuplicates:
     def test_identity(self):
-        input = [0, 1, 2]
-        assert utils.remove_duplicates(input) == input
+        inp = [0, 1, 2]
+        assert utils.remove_duplicates(inp) == inp
 
     def test_empty(self):
         assert utils.remove_duplicates([]) == []
@@ -42,28 +42,33 @@ class TestRemoveDuplicates:
         assert utils.remove_duplicates([1]) == [1]
 
     def test_remove_consecutive(self):
-        input = [0, 1, 1, 2]
-        output = [0, 1, 2]
-        assert utils.remove_duplicates(input) == output
+        inp = [0, 1, 1, 2]
+        out = [0, 1, 2]
+        assert utils.remove_duplicates(inp) == out
 
     def test_remove_gap(self):
-        input = [0, 1, 2, 1]
-        output = [0, 1, 2]
-        assert utils.remove_duplicates(input) == output
+        inp = [0, 1, 2, 1]
+        out = [0, 1, 2]
+        assert utils.remove_duplicates(inp) == out
 
     def test_remove_all_but_one(self):
-        input = [0, 0]
-        output = [0]
-        assert utils.remove_duplicates(input) == output
+        inp = [0, 0]
+        out = [0]
+        assert utils.remove_duplicates(inp) == out
 
 
-class TestSimplyList:
+class TestSimplifyList:
     def test_single_string(self):
-        input = ["test"]
-        output = "test"
-        assert utils.simplify_list(input) == output
+        inp = ["test"]
+        out = "test"
+        assert utils.simplify_list(inp) == out
 
     def test_multi_string(self):
-        input = ["one", "two"]
-        output = ["one", "two"]
-        assert utils.simplify_list(input) == output
+        inp = ["one", "two"]
+        out = ["one", "two"]
+        assert utils.simplify_list(inp) == out
+
+    def test_empty_list(self):
+        inp = []
+        out = []
+        assert utils.simplify_list(inp) == out
