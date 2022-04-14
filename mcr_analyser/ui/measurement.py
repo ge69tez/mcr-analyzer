5a3bb58d449092a74c8b008d9dabaff83a65f354
Martin Knopp <MartinKnopp@gmx.de>
2022-04-14 18:16:05 +0200

    UX: Move grid into hierachical scene struct

--- mcr_analyser/ui/measurement.py
+++ mcr_analyser/ui/measurement.py
@@ -7,6 +7,8 @@
 # This program is free software, see the LICENSE file in the root of this
 # repository for details
 
+import string
+
 from qtpy import QtCore, QtGui, QtWidgets
 import numpy as np
 
@@ -61,7 +63,8 @@ class MeasurementWidget(QtWidgets.QWidget):
         self.scene.addItem(self.image)
         self.view = QtWidgets.QGraphicsView(self.scene)
         self.view.centerOn(meas_width / 2, meas_height / 2)
-        self.spots = []
+        self.grid = None
+
         # Scale result table twice as much as image
         v_layout.addWidget(self.view, 1)
         self.spot_size = QtWidgets.QLineEdit()
@@ -135,36 +138,132 @@ class MeasurementWidget(QtWidgets.QWidget):
             self.results.setModel(self.result_model)
             self.results.resizeColumnsToContents()
 
-            # Remove previous spot markers
-            for spot in self.spots:
-                self.scene.removeItem(spot)
-            self.spots.clear()
-
-            for r in range(measurement.chip.rowCount):
-                for c in range(measurement.chip.columnCount):
-                    valid = utils.simplify_list(
-                        session.query(Result.valid)
-                        .filter_by(measurement=measurement, column=c, row=r)
-                        .one_or_none()
-                    )
-                    pen = QtGui.QPen(QtCore.Qt.GlobalColor.red)
-                    if not valid:
-                        pen.setStyle(QtCore.Qt.DotLine)
-
-                    x = measurement.chip.marginLeft + c * (
-                        measurement.chip.spotSize + measurement.chip.spotMarginHoriz
-                    )
-                    y = measurement.chip.marginTop + r * (
-                        measurement.chip.spotSize + measurement.chip.spotMarginVert
-                    )
-                    self.spots.append(
-                        self.scene.addRect(
-                            x,
-                            y,
-                            measurement.chip.spotSize,
-                            measurement.chip.spotSize,
-                            pen,
-                        )
-                    )
+            if self.grid:
+                self.scene.removeItem(self.grid)
+            self.grid = GridItem(meas_id)
+            self.scene.addItem(self.grid)
+            self.grid.setPos(measurement.chip.marginLeft, measurement.chip.marginTop)
 
             self.image.setPixmap(QtGui.QPixmap.fromImage(qimg))
+
+
+class GridItem(QtWidgets.QGraphicsItem):
+    """Container class for drawing the measurement grid."""
+
+    def __init__(
+        self,
+        meas_id: int,
+        parent=None,
+    ):
+        super().__init__(parent)
+        db = Database()
+        self.session = db.Session()
+        self.measurement = (
+            self.session.query(Measurement)
+            .filter(Measurement.id == meas_id)
+            .one_or_none()
+        )
+        self.cols = self.measurement.chip.columnCount
+        self.rows = self.measurement.chip.rowCount
+        self.vspace = self.measurement.chip.spotMarginVert
+        self.hspace = self.measurement.chip.spotMarginHoriz
+        self.size = self.measurement.chip.spotSize
+
+        self.spots = []
+        self.c_headers = []
+        self.r_headers = []
+        self.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable)
+        self.pen_width = 1.0
+
+        self._add_children()
+
+    def boundingRect(self):
+        width = (
+            self.cols * self.size + (self.cols - 1) * self.vspace + self.pen_width / 2
+        )
+        height = (
+            self.rows * self.size + (self.rows - 1) * self.hspace + self.pen_width / 2
+        )
+        # Labels are drawn on the "negative" side of the origin
+        return QtCore.QRectF(
+            -self.pen_width / 2 - 15, -self.pen_width / 2 - 15, width, height
+        )
+
+    def _clear_children(self):
+        for head in self.c_headers:
+            self.removeItem(head)
+        self.c_headers.clear()
+
+        for head in self.r_headers:
+            self.removeItem(head)
+        self.r_headers.clear()
+
+        for spot in self.spots:
+            self.removeItem(spot)
+        self.spots.clear()
+
+    def _add_children(self):
+        # Row lables: letters
+        for row in range(self.rows):
+            head = GraphicsRectTextItem(
+                -15,
+                row * (self.size + self.hspace),
+                12,
+                self.size,
+                string.ascii_uppercase[row],
+                self,
+            )
+            self.r_headers.append(head)
+
+        for col in range(self.cols):
+            # Column labels
+            head = GraphicsRectTextItem(
+                col * (self.size + self.vspace), -15, self.size, 12, str(col + 1), self
+            )
+            self.c_headers.append(head)
+
+            for row in range(self.rows):
+                valid = utils.simplify_list(
+                    self.session.query(Result.valid)
+                    .filter_by(measurement=self.measurement, column=col, row=row)
+                    .one_or_none()
+                )
+                pen = QtGui.QPen(QtCore.Qt.GlobalColor.red)
+                if not valid:
+                    pen.setStyle(QtCore.Qt.DotLine)
+
+                x = col * (self.size + self.vspace)
+                y = row * (self.size + self.hspace)
+                spot = QtWidgets.QGraphicsRectItem(x, y, self.size, self.size, self)
+                spot.setPen(pen)
+                self.spots.append(spot)
+
+
+class GraphicsRectTextItem(QtWidgets.QGraphicsRectItem):
+    """Draws text on a rectangular background."""
+
+    def __init__(
+        self,
+        x: float,
+        y: float,
+        w: float,
+        h: float,
+        t: str,
+        parent,
+    ) -> None:
+        super().__init__(x, y, w, h, parent)
+        self.text = t
+        self.setPen(QtCore.Qt.GlobalColor.white)
+        self.setBrush(QtCore.Qt.GlobalColor.white)
+
+    def paint(
+        self,
+        painter: QtGui.QPainter,
+        option,
+        widget,
+    ) -> None:
+        super().paint(painter, option, widget)
+        painter.setPen(QtCore.Qt.GlobalColor.black)
+        painter.drawText(
+            option.rect, QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter, self.text
+        )
