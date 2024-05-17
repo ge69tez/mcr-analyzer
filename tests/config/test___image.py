3df8b4e51f0fc8f8f47591f45949c1b90ee9487b
ge69tez <142658434+ge69tez@users.noreply.github.com>
2024-05-17 12:14:11 +0200

    test: remove generation of random rotation angle
    - Low numerical stability

--- tests/config/test___image.py
+++ tests/config/test___image.py
@@ -1,5 +1,4 @@
 from datetime import timedelta
-from math import cos, radians, sin
 from secrets import SystemRandom
 
 import numpy as np
@@ -27,11 +26,8 @@ random.seed(0)
 def image_row_count_column_count(draw: st.DrawFn) -> tuple[OPEN_CV__IMAGE__ND_ARRAY__DATA_TYPE, int, int]:
     row_count = draw(st.integers(min_value=5, max_value=26))
     column_count = draw(st.integers(min_value=5, max_value=26))
-    rotation_angle_in_degree = draw(st.integers(min_value=-5, max_value=5))
 
-    image = _generate_random_image(
-        row_count=row_count, column_count=column_count, rotation_angle_in_degree=rotation_angle_in_degree
-    )
+    image = _generate_random_image(row_count=row_count, column_count=column_count)
 
     return image, row_count, column_count
 
@@ -49,8 +45,8 @@ def test___config__image(image_row_count_column_count: tuple[OPEN_CV__IMAGE__ND_
     )
 
     # - Allow some calculation errors
-    assert abs(column_count_result - column_count) <= 1
-    assert abs(row_count_result - row_count) <= 1
+    assert column_count_result == column_count
+    assert row_count_result == row_count
 
 
 def test___config__image__black_and_white() -> None:
@@ -62,9 +58,7 @@ def test___config__image__black_and_white() -> None:
         assert grid_result.failure() == "Spot list by roundness is empty."
 
 
-def _generate_random_image(
-    *, row_count: int, column_count: int, rotation_angle_in_degree: int
-) -> OPEN_CV__IMAGE__ND_ARRAY__DATA_TYPE:
+def _generate_random_image(*, row_count: int, column_count: int) -> OPEN_CV__IMAGE__ND_ARRAY__DATA_TYPE:
     height, width = (PGM__HEIGHT, PGM__WIDTH)
 
     interval_row = height / (row_count + 1)
@@ -92,35 +86,11 @@ def _generate_random_image(
         for j in range(column_count):
             x = margin_width + interval_column * j
 
-            x_after_rotation, y_after_rotation = _rotate_point(
-                rotation_angle_in_degree=rotation_angle_in_degree, center=(width / 2, height / 2), point=(x, y)
-            )
-
-            spot_list.append((Position(x_after_rotation, y_after_rotation), spot_radius))
+            spot_list.append((Position(x, y), spot_radius))
 
             spot_count += 1
 
     return draw_circle_on_image_like(image=image, spot_with_radius_list=spot_list)
-
-
-def _rotate_point(
-    rotation_angle_in_degree: int, center: tuple[float, float], point: tuple[float, float]
-) -> tuple[float, float]:
-    x, y = point
-    center_x, center_y = center
-    cos_theta = cos(radians(rotation_angle_in_degree))
-    sin_theta = sin(radians(rotation_angle_in_degree))
-
-    x_relative_to_center = x - center_x
-    y_relative_to_center = y - center_y
-
-    x_relative_to_center_after_rotation = x_relative_to_center * cos_theta - y_relative_to_center * sin_theta
-    y_relative_to_center_after_rotation = y_relative_to_center * cos_theta + x_relative_to_center * sin_theta
-
-    x_after_rotation = x_relative_to_center_after_rotation + center_x
-    y_after_rotation = y_relative_to_center_after_rotation + center_y
-
-    return x_after_rotation, y_after_rotation
 
 
 @given(image_row_count_column_count())
